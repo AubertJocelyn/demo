a0bbc12c7d37681aead9828e79d4016260ced3e8
Vincent <vincent.lugardon@telecom-sudparis.eu>
2025-05-23 19:21:02 +0200

    ça a marché

--- src/main/python/module_de_tri/fonctionTri.py
+++ src/main/python/module_de_tri/fonctionTri.py
@@ -1,5 +1,6 @@
 # librairie utiles
-from . import annexe as anx
+import os
+import annexe as anx
 from PIL import Image
 from PIL.ExifTags import TAGS
 import numpy as np
@@ -13,14 +14,14 @@ import cv2
 # Récupère les métadonnées EXIF d'une image
 
 # Tri par année à partir de la date EXIF
-def tri_date_annee(image_filename):
+def annees(image_filename):
     date = anx.metadonnees(image_filename).get('DateTime')
     if date:  #test si données existe dans EXIF
         return date.split(":")[0]
     return "Sans date"
 
 #Tri par nom_appareil à partir des données EXIF
-def tri_appareil(nom):
+def appareil(nom):
     image = Image.open(nom)
     mar = anx.metadonnees(image).get('Make')
     mol = anx.metadonnees(image).get('Model')
@@ -30,15 +31,22 @@ def tri_appareil(nom):
         return mar
     return "Sans modele"
 
-def tri_date_mois (nom):
+def mois (nom):
     image = Image.open(nom)
     date = anx.metadonnees(image).get('DateTime')
     if date:
         return anx.mois[date.split(":")[1]+1]
     return "Sans date"
 
+def jour (nom):
+    image = Image.open(nom)
+    date = anx.metadonnees(image).get('DateTime')
+    if date:
+        return date.split(":")[2]
+    return "Sans date"
+
 # Tri par heure à partir de la date EXIF
-def tri_heure (nom):
+def heure (nom):
     image = Image.open(nom)
     date = anx.metadonnees(image).get('DateTime')
     if date:
@@ -47,7 +55,7 @@ def tri_heure (nom):
     return "Sans date"
 
 # Tri par distinction jour/nuit, il regarde l'heure (jour compris entre 7h et 20h) à partir de la date EXIF
-def tri_jour_nuit (nom):
+def jour_nuit (nom):
     image = Image.open(nom)
     date = anx.metadonnees(image).get('DateTime')
     if date:
@@ -60,7 +68,7 @@ def tri_jour_nuit (nom):
 
 
 # Tri par couleur (R=0, B=1, V=2) somme tous les pixel et fais la moyenne renvoit toute ces couleurs: rouge vert bleu blanc jaune noir cyan magenta
-def tri_couleur(nom):
+def couleur(nom):
     image = Image.open(nom)
     a = np.sum(np.array(image), axis=(0, 1))  # Somme par canal R, G, B
     a=[int(i/(len(np.array(image))*len(np.array(image)[0]))) for i in a] #moyenne
@@ -82,7 +90,7 @@ def tri_couleur(nom):
 
 # Tri par localisation à partir de la date utc: compare, l'heure local et utc pour definir le fuseau horaire corespondant, prend uniquement les décalages entier ignore les demis comme pour l'inde.
 # le dictionnaire définit dans les variable fait le lien entre le fuseau horaire et la localisation
-def decalage_utc(nom):
+def localisation(nom):
     image = Image.open(nom)
     date = anx.metadonnees(image).get('DateTime')
     if date:
@@ -95,18 +103,20 @@ def decalage_utc(nom):
                     return anx.fuseau_horaire.get(decalage)# utilisation du dictionnaire
     return "Sans date"
 
-def tri_presence_visage(image):
-    image = cv2.imread(image)
+def presence_visage(fil):
+    image=Image.open(fil)
     # on convertit l'image en noir et blanc
     # l'algorithme que nous allons utilisé a besoin de ce pretraitement
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_gray = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
     #https://github.com/opencv/opencv/tree/master/data/haarcascades
     # on charge notre modèle
-    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    demo_directory = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
+    base_path = os.path.join("main", "python", "module_de_tri")
+    cascade_file = os.path.join(demo_directory,base_path, "haarcascade_frontalface_alt.xml")
+    face_cascade = cv2.CascadeClassifier(cascade_file)
     # On cherche tous les visages disponibles dans l'image
     faces = face_cascade.detectMultiScale(image_gray, 1.05, 5,minSize=(50, 50))
     # on écrit dans la console le nombre de visages que  l'algorithme a détecté
-    print( len(faces))
     if len(faces)>0:
         return "avec"
     return "sans"
